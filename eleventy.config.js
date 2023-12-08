443923616c1b5ccb49a9109559fe2071b8278460
Joe Shoop <shoopj@gmail.com>
2023-12-07 16:42:29 -0800

    initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,34 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setServerOptions({
     watch: ['_site/**/*.css'],
   });
+  
+  eleventyConfig.addCollection("work", function(collection) {
+    const coll = collection.getFilteredByTag("work");
+  
+    for(let i = 0; i < coll.length ; i++) {
+      const prevWork = coll[i-1];
+      const nextWork = coll[i + 1];
+  
+      coll[i].data["prevWork"] = prevWork;
+      coll[i].data["nextWork"] = nextWork;
+    }
+  
+    return coll;
+  });
 
   return {
     dir: {
       input: 'src',
+      includes: "_includes"
     },
+    templateFormats: [
+      "liquid",
+      "css",
+      "jpg",
+      "png",
+      "svg",
+      "mp4",
+      "css" // css is not yet a recognized template extension in Eleventy
+    ]
   };
 };
